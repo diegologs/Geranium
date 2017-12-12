3e9b5ed17b56d7c3934356245294f5febbdf8243
Frostqui <diego.lopezgarcia@hotmail.com>
2017-12-12 13:55:56 +0100

    Created main window with JFrame

--- src/Geranium.java
+++ src/Geranium.java
@@ -1,9 +1,46 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Geranium {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Geranium extends Canvas{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 420;
+	public static final int HEIGHT = WIDTH / 16 * 9;
+	public static final int SCALE = 3;
+	public static final String TITLE = "Geranium";
+	
+	private JFrame frame;
+	
+	public Geranium() {
+		
+		Dimension dim = new Dimension(WIDTH * SCALE, HEIGHT * SCALE );
+		setPreferredSize(dim);
+		frame = new JFrame();
+		
+	}
 
 	public static void main(String args[]) {
-		System.out.println("Testing");
+		createWindow();
 	}
 	
+	public static void createWindow() {
+	
+		Geranium geranium = new Geranium();
+		geranium.frame.setTitle("Geranium");
+		geranium.frame.add(geranium);
+		geranium.frame.pack();
+		geranium.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		geranium.frame.setVisible(true);
+		geranium.frame.setLocationRelativeTo(null);
+		
+		
+		
+	}
 	
 }
